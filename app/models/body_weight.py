38b200f4d64ff6efd21fc6cc6e024b47edc917f0
bruno0564 <bruno20051121@gmail.com>
2026-05-18 18:07:46 +0200

    chore: eliminar comentarios obvios del código

--- app/models/body_weight.py
+++ app/models/body_weight.py
@@ -6,7 +6,6 @@ from app.database import Base
 
 class BodyWeight(Base):
     __tablename__ = "body_weight"
-    #Mapped[tipo] forma de defninir  una columna
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     weight: Mapped[float] = mapped_column(Float, nullable=False)
     date: Mapped[date] = mapped_column(Date, nullable=False)
